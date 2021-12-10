20bdb7b36b576dcdcbd2813a0a4d92a9b8a141b8
rodriguesss <viniciusrdefreitas@gmail.com>
2021-12-10 15:39:02 -0300

    add data with more five objects

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,26 @@ export default function App() {
     {
       question: 'Componentes devem iniciar com __',
       answer: `letra maiúscula`
+    },
+    {
+      question: 'Podemos colocar __ dentro do JSX',
+      answer: `expressões`
+    },
+    {
+      question: 'O ReactDOM nos ajuda __',
+      answer: `interagindo com a DOM para colocar componentes React na mesma`
+    },
+    {
+      question: 'Usamos o npm para __',
+      answer: `gerenciar os pacotes necessários e suas dependências`
+    },
+    {
+      question: 'Usamos props para __',
+      answer: `passar diferentes informações para componentes`
+    },
+    {
+      question: 'Usamos estado (state) para __',
+      answer: `dizer para o React quais informações quando atualizadas devem renderizar a tela novamente`
     }
   ])
 
